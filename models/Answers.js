d01e65ae989770bb0418164a1d0f87c75a280035
POPSICKLES <popsickles.py@gmail.com>
2021-10-29 03:22:01 -0400

    working answers

--- models/Answers.js
+++ models/Answers.js
@@ -1,30 +1,32 @@
 module.exports = (sequelize, DataTypes) => {
-    const Answers = sequelize.define("Answers", {
-        answer_id: {
-            type: DataTypes.BIGINT,
-            allowNull: false,
-            autoIncrement: true,
-            primaryKey: true,
-        },
-        question_id:{
-            type: DataTypes.BIGINT,
-            allowNull: false,
-            foreignKey: true,
-        },
-        answer_text: {
-            type: DataTypes.STRING,
-            allowNull: false,
-        },
-        is_correct: {
-            type: DataTypes.BOOLEAN,
-            allowNull: false,
-        },
-    })
+  const Answers = sequelize.define("Answers", {
+    answer_id: {
+      type: DataTypes.BIGINT,
+      allowNull: false,
+      autoIncrement: true,
+      primaryKey: true,
+    },
+    question_id: {
+      type: DataTypes.BIGINT,
+      allowNull: false,
+      foreignKey: true,
+    },
+    answer_text: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    is_correct: {
+      type: DataTypes.BOOLEAN,
+      allowNull: false,
+    },
+  })
 
-    Answers.associate = (models) => {
-        Answers.belongsTo(models.Questions, {
-            foreignKey: 'question_id'
-        });
-    }
-    return Answers;
+  Answers.associate = (models) => {
+    Answers.belongsTo(models.Questions, {
+      foreignKey: 'question_id',
+      onDelete: 'cascade',
+      hooks: true,
+    });
+  }
+  return Answers;
 }
