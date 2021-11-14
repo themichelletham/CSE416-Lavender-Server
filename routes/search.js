82ea36a52baa1e2db64497a0dfdc57b567f5dbad
Judy Liu <judy.liu.1@stonybrook.edu>
2021-11-14 10:51:13 -0500

    routing for search in home page

--- routes/search.js
+++ routes/search.js
@@ -1,10 +1,27 @@
 const express = require("express");
 const router = express.Router();
-const { Quizzes } = require ("../models");
+const { Quizzes, Platforms } = require("../models");
+const { Op } = require("sequelize");
 
+router.get("/:keyword", async (req, res) => {
+  //res.send("Platform Quiz Search");
+  keyword = req.params.keyword;
+  const platforms = await Platforms.findAll({
+    where: {
+      platform_name: {
+        [Op.like]: "%" + keyword + "%",
+      },
+    },
+  });
+  const quizzes = await Quizzes.findAll({
+    where: {
+      quiz_name: {
+        [Op.like]: "%" + keyword + "%",
+      },
+    },
+  });
 
-router.get("/", (req, res) =>{
-    res.send("Hello search");
+  res.json({ platforms: platforms, quizzes: quizzes });
 });
 
-module.exports = router
+module.exports = router;
