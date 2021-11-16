36e5f22401d6ea0d381765283b9b38e71bdbcda6
Judy Liu <judy.liu.1@stonybrook.edu>
2021-11-16 12:23:51 -0500

    added search queries

--- routes/search.js
+++ routes/search.js
@@ -3,24 +3,36 @@ const router = express.Router();
 const { Quizzes, Platforms } = require("../models");
 const { Op } = require("sequelize");
 
+router.get("/", async (req, res) => {
+  const quizzes = await Quizzes.findAll();
+  const platforms = await Platforms.findAll();
+  res.status(200).send({ quizzes: quizzes, platforms: platforms });
+});
+
 router.get("/:keyword", async (req, res) => {
   //res.send("Platform Quiz Search");
-  keyword = req.params.keyword;
-  const platforms = await Platforms.findAll({
-    where: {
-      platform_name: {
-        [Op.like]: "%" + keyword + "%",
+  const keyword = req.params.keyword;
+  let quizzes = null;
+  let platforms = null;
+  if (keyword == "undefined") {
+    quizzes = await Quizzes.findAll();
+    platforms = await Platforms.findAll();
+  } else {
+    platforms = await Platforms.findAll({
+      where: {
+        platform_name: {
+          [Op.like]: "%" + keyword + "%",
+        },
       },
-    },
-  });
-  const quizzes = await Quizzes.findAll({
-    where: {
-      quiz_name: {
-        [Op.like]: "%" + keyword + "%",
+    });
+    quizzes = await Quizzes.findAll({
+      where: {
+        quiz_name: {
+          [Op.like]: "%" + keyword + "%",
+        },
       },
-    },
-  });
-
+    });
+  }
   res.json({ platforms: platforms, quizzes: quizzes });
 });
 
