bc0fbf9ede7cbf6d1a08016b58f00850d18b2ef3
Judy Liu <judy.liu.1@stonybrook.edu>
2021-11-16 12:23:51 -0500

    added search queries

--- index.js
+++ index.js
@@ -1,28 +1,30 @@
-const express = require('express');
-const passport = require('passport');
-const cors = require('cors');
+const express = require("express");
+const passport = require("passport");
+const cors = require("cors");
 const app = express();
-const cookieSession = require('cookie-session');
-const env = process.env.NODE_ENV || 'development';
-const config = require(__dirname + '/config/config.json')[env];
-require('./auth/googleSSO');
+const cookieSession = require("cookie-session");
+const env = process.env.NODE_ENV || "development";
+const config = require(__dirname + "/config/config.json")[env];
+require("./auth/googleSSO");
 
-app.use(express.json({limit: '50mb'}));
-app.use(express.urlencoded({limit: '50mb', extended: true}));
+app.use(express.json({ limit: "50mb" }));
+app.use(express.urlencoded({ limit: "50mb", extended: true }));
 app.use(cors({ origin: config.clientUrl, credentials: true }));
-app.use(cookieSession({
-  maxAge: 24*60*60*1000,
-  keys: ['randomSalt'], //replace with bcrypt or something
-  sameSite: 'none'
-}))
+app.use(
+  cookieSession({
+    maxAge: 24 * 60 * 60 * 1000,
+    keys: ["randomSalt"], //replace with bcrypt or something
+    sameSite: "none",
+  })
+);
 
 const db = require("./models");
 
 //Routers
-const authRouter = require('./routes/auth');
+const authRouter = require("./routes/auth");
 app.use(passport.initialize());
 app.use(passport.session());
-app.use('/auth', authRouter);
+app.use("/auth", authRouter);
 
 const platformRouter = require("./routes/platform");
 app.use("/platform", platformRouter);
@@ -36,12 +38,13 @@ app.use("/quiz", quizRouter);
 const searchRouter = require("./routes/search");
 app.use("/search", searchRouter);
 
-db.sequelize.sync().then(() => {
-
+db.sequelize
+  .sync()
+  .then(() => {
     app.listen(process.env.PORT || 3001, () => {
-        console.log("Server running on port 3001");
+      console.log("Server running on port 3001");
     });
-})
-.catch((err) => {
+  })
+  .catch((err) => {
     console.log(err);
-});
+  });
