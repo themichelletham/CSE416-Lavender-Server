8d13df91eb1f7453a044c723af4d31a9581ca07d
POPSICKLES <popsickles.py@gmail.com>
2021-12-02 14:46:32 -0500

    protected platform edit

--- routes/platform.js
+++ routes/platform.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const router = express.Router();
+const { isAuthenticated } = require("../auth/middlewares");
 const { Op } = require("sequelize");
 const { Quizzes, Platforms, Users } = require("../models");
 
@@ -17,7 +18,8 @@ router.post("/", async (req, res) => {
   res.status(201).json(platform);
 });
 
-router.get("/:platform_id", async (req, res) => {
+router.get("/:platform_id", isAuthenticated, async (req, res) => {
+  const user = req.user;
   const keyword = req.query.keyword;
   const platform = await Platforms.findOne({
     where: { platform_id: req.params.platform_id },
@@ -28,6 +30,11 @@ router.get("/:platform_id", async (req, res) => {
     res.sendStatus(404);
     return;
   }
+  if(user.user_id != platform.user_id){
+    res.sendStatus(401);
+    return;
+  }
+  
   const quizzes = await platform
     .getQuizzes({
       where: {
@@ -74,6 +81,7 @@ router.get("/:platform_id", async (req, res) => {
   }
   res.json({
     platform_name: platform.platform_name,
+    user_id: platform.user_id,
     icon_photo: platform.icon_photo,
     banner_photo: platform.banner_photo,
     quizzes: quizzes,
@@ -97,9 +105,18 @@ router.delete("/:platform_id", async (req, res) => {
   res.sendStatus(204);
 });
 
-router.put("/:platform_id/creator", async (req, res) => {
-  //res.send('Platform Update');
-  const platform_fields = req.body.platform_fields;
+router.put("/:platform_id/creator", isAuthenticated, async (req, res) => {
+  const user = req.user;
+  const platform = await user.getPlatform();
+  console.log(platform.platform_id)
+  console.log(req.params.platform_id);
+  if(platform.platform_id != req.params.platform_id){
+    res.sendStatus(401);
+    return;
+  }
+
+  const platform_fields = {};
+  platform_fields.platform_name = req.body.platform_fields.platform_name;
   await Platforms.update(platform_fields, {
     where: {
       platform_id: req.params.platform_id,
